9123c5c1be20a5972e9985fd1f4213439f143d99
JuannMaicon <jmaicon308@gmail.com>
2024-10-01 09:08:20 -0300

    class No

--- EstruturaDados/src/lista/ListaEncadeada.java
+++ EstruturaDados/src/lista/ListaEncadeada.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lista;
 
-/**
- *
- * @author juan.masantos
- */
-public class ListaEncadeada {
-    
+public class ListaEncadeada<Tipo> {
+
+    private No inicio;
+    private No fim;
+    private int tamanho;
+
+    //Método Construtor//
+    public ListaEncadeada() {
+
+        this.tamanho = 0;
+    }
+    //Métodos De Acesso//
+    public No getInicio() {
+        return inicio;
+    }
+
+    public No getFim() {
+        return fim;
+    }
+
+    public int getTamanho() {
+        return tamanho;
+    }
+
+    public void setInicio(No inicio) {
+        this.inicio = inicio;
+    }
+
+    public void setFim(No fim) {
+        this.fim = fim;
+    }
+
+    public void setTamanho(int tamanho) {
+        this.tamanho = tamanho;
+    }
+    //Método De Exibição
+    @Override
+    public String toString() {
+        return "ListaEncadeada{" + "inicio=" + inicio + "}";
+    }
+    //Método Para Adionar Elemento//
+    public void adiciona (Tipo elemento){
+        No<Tipo> celula = new No<Tipo>(elemento);
+        if(this.inicio == null && this.fim == null){
+            this.inicio = celula;
+            this.fim = celula;
+        }
+        else{
+            this.fim.setProximo(celula);
+            this.fim = celula;
+        }
+        this.tamanho++;
+    }
+
 }
--- EstruturaDados/src/lista/No.java
+++ EstruturaDados/src/lista/No.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lista;
 
-/**
- *
- * @author juan.masantos
- */
-public class No {
+public class No<Tipo> {
+    private Tipo elemento;
+    private No<Tipo> proximo;
+    
+    public No(Tipo elemento){
+        this.elemento =  elemento;
+        this.proximo = null;
+    }
+    public No(Tipo elemento, No<Tipo> proximo){
+        this.elemento = elemento;
+        this.proximo = proximo;
+    }
+
+    public Tipo getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Tipo elemento) {
+        this.elemento = elemento;
+    }
+
+    public No<Tipo> getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(No<Tipo> proximo) {
+        this.proximo = proximo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" + "elemento=" + elemento + ", proximo=" + proximo + '}';
+    }
+    
+    
     
 }
